9b31ed862dc2287fab0fd2aca7d0606db6bdfe29
mzums <annsie035@gmail.com>
2026-07-05 19:53:58 +0200

    first webpage

---
+++ web/src/App.tsx
@@ -0,0 +1,55 @@
+import './index.css'
+
+function App() {
+  return (
+    <>
+      <section id="welcome">
+        <h1>Welcome to FakeWiki,</h1>
+        <h3>the fake encyclopedia maintained by mzums</h3>
+        <h3>Articles generated using data from Wikipedia (learn more)</h3>
+      </section>
+
+      <section id="card">
+        <section id="card-title">
+          From today's featured article
+        </section>
+        <p id="card-text">
+          The name of the computer game console and the game console is a software that combines the original system of the same name, which can be used in the computer game console. It was developed in the early 1990s by the IBM Computer Engine, and developed in 1992.
+
+        </p>
+      </section>
+
+      <section id="card">
+        <section id="card-title">
+          In the news
+        </section>
+        <p id="card-text">
+          The name of the computer game console and the game console is a software that combines the original system of the same name, which can be used in the computer game console. It was developed in the early 1990s by the IBM Computer Engine, and developed in 1992.
+
+        </p>
+      </section>
+
+      <section id="card">
+        <section id="card-title">
+          Did you know ...
+        </section>
+        <p id="card-text">
+          The name of the computer game console and the game console is a software that combines the original system of the same name, which can be used in the computer game console. It was developed in the early 1990s by the IBM Computer Engine, and developed in 1992.
+
+        </p>
+      </section>
+
+      <section id="card">
+        <section id="card-title">
+          On this day
+        </section>
+        <p id="card-text">
+          The name of the computer game console and the game console is a software that combines the original system of the same name, which can be used in the computer game console. It was developed in the early 1990s by the IBM Computer Engine, and developed in 1992.
+
+        </p>
+      </section>
+    </>
+  )
+}
+
+export default App
