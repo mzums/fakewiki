0b6517bacff456f46ee791935110e2f15f837f68
mzums <annsie035@gmail.com>
2026-07-05 22:32:08 +0200

    added footer, header and light/dark mode switching

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,54 +1,13 @@
-import './index.css'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Main_Page'
+import About from './pages/All_Articles'
 
 function App() {
   return (
-    <>
-      <section id="welcome">
-        <h1>Welcome to FakeWiki,</h1>
-        <h3>the fake encyclopedia maintained by mzums</h3>
-        <h3>Articles generated using data from Wikipedia (learn more)</h3>
-      </section>
-
-      <section id="card">
-        <section id="card-title">
-          From today's featured article
-        </section>
-        <p id="card-text">
-          The name of the computer game console and the game console is a software that combines the original system of the same name, which can be used in the computer game console. It was developed in the early 1990s by the IBM Computer Engine, and developed in 1992.
-
-        </p>
-      </section>
-
-      <section id="card">
-        <section id="card-title">
-          In the news
-        </section>
-        <p id="card-text">
-          The name of the computer game console and the game console is a software that combines the original system of the same name, which can be used in the computer game console. It was developed in the early 1990s by the IBM Computer Engine, and developed in 1992.
-
-        </p>
-      </section>
-
-      <section id="card">
-        <section id="card-title">
-          Did you know ...
-        </section>
-        <p id="card-text">
-          The name of the computer game console and the game console is a software that combines the original system of the same name, which can be used in the computer game console. It was developed in the early 1990s by the IBM Computer Engine, and developed in 1992.
-
-        </p>
-      </section>
-
-      <section id="card">
-        <section id="card-title">
-          On this day
-        </section>
-        <p id="card-text">
-          The name of the computer game console and the game console is a software that combines the original system of the same name, which can be used in the computer game console. It was developed in the early 1990s by the IBM Computer Engine, and developed in 1992.
-
-        </p>
-      </section>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
   )
 }
 
